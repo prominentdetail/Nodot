7361818fea232d52e191169a63dce8c28f88d940
prominent <thekonspiracy@hotmail.com>
2017-08-08 15:05:08 -0500

    checking if getItem is sufficient
    checking if getItem is sufficient

--- js/colorPanel.js
+++ js/colorPanel.js
@@ -8,16 +8,10 @@ function colorData(){
 	this.currentColor = 0;
 	
 	this.init = function(){
-		try{
-			this.picked[0] = JSON.parse(localStorage.primary) || {color:'rgba(0,255,0,1)', e:null, r:0,g:0,b:0, h:0,s:0,l:0};
-			this.picked[1] = JSON.parse(localStorage.secondary) || {color:'rgba(255,255,255,1)', e:null, r:255,g:255,b:255, h:0,s:100,l:100};
-		}
-		catch(e){
-			this.picked[0] = {color:'rgba(0,255,0,1)', e:null, r:0,g:0,b:0, h:0,s:0,l:0};
-			this.picked[1] = {color:'rgba(255,255,255,1)', e:null, r:255,g:255,b:255, h:0,s:100,l:100};
-			//for some reason I had to do a try/catch because github was giving an error when trying to access a localStorage variable that hadn't been assigned a value yet.
-		}
-		
+		this.picked[0] = JSON.parse(localStorage.getItem("primary")) || {color:'rgba(0,255,0,1)', e:null, r:0,g:0,b:0, h:0,s:0,l:0};
+		this.picked[1] = JSON.parse(localStorage.getItem("secondary")) || {color:'rgba(255,255,255,1)', e:null, r:255,g:255,b:255, h:0,s:100,l:100};
+		//github was giving an error when trying to access a localStorage variable that hadn't been assigned a value yet. Trying getItem, so see if it solves the issue.
+				
 		master.e.colorPanel.innerHTML = '\
 			<table width="100%"><tr><td>\
 				<button id="rgbhslButton" class="buttonIcon ui-rgb-icon" onclick="toggleIcon(this)">&nbsp;</button>\
